f08cbba2fdb52833e2a9026dba68bf2961cece8d
Ghulam Akbar <ghulamakbarabbbro110@gmail.com>
2024-03-30 10:04:25 +0500

    1.0.0

---
+++ index.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([{
+        name: "numberGuess",
+        Type: "number",
+        message: "Please guess a number between 1 to 5",
+    },]);
+if (answer.numberGuess == randomNumber) {
+    console.log("You guess the correct number");
+}
+else {
+    console.log("You guess the wrong number");
+}
+;
